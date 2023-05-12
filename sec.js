0f80441a0fb528a59e988cd2e4e475605efac635
rajendra mandliya <119125519+mandliyarajendra11@users.noreply.github.com>
2023-05-12 17:42:34 +0530

    css changes

--- sec.js
+++ sec.js
@@ -1,3 +1,6 @@
 const url= new URLSearchParams(window.location.search)
 const val=url.get('val');
+const loc=url.get('location');
+const salary=url.get('salary')
 document.getElementById('head').innerText=val
+document.querySelector('.card-header').innerText=`  Location- ${loc} / Salary- ${salary}`
